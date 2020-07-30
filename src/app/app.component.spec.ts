0e31514c7ab704bf7ff92d92b9a09d8612e24d50
SharvanSaini <500063736@stu.upes.ac.in>
2020-07-31 00:50:24 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CovidTracker'`, () => {
+  it(`should have as title 'covid'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CovidTracker');
+    expect(app.title).toEqual('covid');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('CovidTracker app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('covid app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { CountriesComponent } from './component/countries/countries.component';
+//import { DataServiceComponent } from './services/data-service/data-service.component';
+import {HttpClientModule} from '@angular/common/http';
+import { DashboardCardComponent } from './component/dashboard-card/dashboard-card.component';
+import { GoogleChartsModule } from 'angular-google-charts';
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent,
+    DashboardCardComponent,
+  //  DataServiceComponent
+ ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
